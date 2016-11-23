399f11b4c7b44e292d89ed789da09bcdbbef540f
Arnon Sela <arnon@acrisel.com>
2016-11-23 13:21:56 -0600

    add logger for mp with rotation

--- acris/acris/__init__.py
+++ acris/acris/__init__.py
@@ -1,5 +1,7 @@
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 from .threaded import threaded
 from .singleton import Singleton
 from .sequence import Sequence
+from .timed_sized_logging_handler import TimedSizedRotatingHandler
+from .mplogger import MpLogger
