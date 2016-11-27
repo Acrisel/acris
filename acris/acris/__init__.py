b01ccc78d28d935eb7078bff6b33a19b0bfcd12c
Arnon Sela <arnon@acrisel.com>
2016-11-27 17:39:03 -0600

    add MergeChainedDict

--- acris/acris/__init__.py
+++ acris/acris/__init__.py
@@ -1,7 +1,8 @@
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 
 from .threaded import threaded
 from .singleton import Singleton
 from .sequence import Sequence
 from .timed_sized_logging_handler import TimedSizedRotatingHandler
 from .mplogger import MpLogger
+from .data_types import MergedChainedDict
